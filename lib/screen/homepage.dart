bca00b73b96e66c87ce5cecc6b0b04aec2a58d9c
Hilbert Tan <htan_220000001144@uic.edu.ph>
2024-12-02 19:36:42 +0800

    Hookwidget

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:midterm_project/screen/screen1.dart';
-import 'package:midterm_project/screen/screen2.dart';
-import 'package:midterm_project/screen/screen3.dart';
-import 'package:midterm_project/screen/screen4.dart';
-import 'package:midterm_project/screen/screen5.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final counterList = useState([1, 2, 3, 4]);
+
+    void addCounter(int position) {
+      if (position == 0) {
+        if (counterList.value[0] == 9) {
+          counterList.value[0] = 0;
+        } else {
+          counterList.value[0] += 1;
+        }
+      } else if (position == 1) {
+      } else if (position == 2) {
+      } else {}
+    }
+
     return MaterialApp(
         home: Scaffold(
       backgroundColor: Colors.black,
@@ -120,14 +130,15 @@ class HomePage extends StatelessWidget {
                     children: <Widget>[
                       ElevatedButton(
                         onPressed: () {
-                          print("ok");
+                          addCounter(0);
+                          print(counterList.value[0]);
                         },
                         child: Text(
                           "up",
                           style: TextStyle(
                             color: const Color.fromARGB(255, 43, 91, 16),
                             fontFamily: 'PixelifySans',
-                            fontSize: 15,
+                            fontSize: 25,
                           ),
                         ),
                         style: ElevatedButton.styleFrom(
@@ -145,8 +156,8 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       Container(
-                        width: 50,
-                        height: 50,
+                        width: 75,
+                        height: 75,
                         decoration: BoxDecoration(
                           color: Colors.black,
                           border: Border.all(
@@ -156,20 +167,322 @@ class HomePage extends StatelessWidget {
                         ),
                         child: Center(
                           child: Text(
-                            "0",
+                            "${counterList.value[0]}",
                             style: TextStyle(
                               color: const Color.fromARGB(255, 43, 91, 16),
                               fontFamily: 'PixelifySans',
-                              fontSize: 25, // Large font to fill the space
+                              fontSize: 45, // Large font to fill the space
                             ),
                             textAlign:
                                 TextAlign.center, // Ensure the text is centered
                           ),
                         ),
-                      )
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "down",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
                     ],
-                  )
+                  ),
+                  Column(
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "up",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            width: 5,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${counterList.value[1]}",
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 43, 91, 16),
+                              fontFamily: 'PixelifySans',
+                              fontSize: 45, // Large font to fill the space
+                            ),
+                            textAlign:
+                                TextAlign.center, // Ensure the text is centered
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "down",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "up",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            width: 5,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${counterList.value[2]}",
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 43, 91, 16),
+                              fontFamily: 'PixelifySans',
+                              fontSize: 45, // Large font to fill the space
+                            ),
+                            textAlign:
+                                TextAlign.center, // Ensure the text is centered
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "down",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "up",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
+                      Container(
+                        width: 75,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            width: 5,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${counterList.value[3]}",
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 43, 91, 16),
+                              fontFamily: 'PixelifySans',
+                              fontSize: 45, // Large font to fill the space
+                            ),
+                            textAlign:
+                                TextAlign.center, // Ensure the text is centered
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print("ok");
+                        },
+                        child: Text(
+                          "down",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 43, 91, 16),
+                            fontFamily: 'PixelifySans',
+                            fontSize: 25,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.black, // Set the background color to black
+                          // Set text color (optional, already set in Text)
+                        ).copyWith(
+                          // Disable hover effects
+                          elevation: MaterialStateProperty.all(
+                              0), // No shadow on hover
+                          overlayColor: MaterialStateProperty.all(Colors
+                              .transparent), // No hover effect color change
+                          splashFactory:
+                              NoSplash.splashFactory, // Disable splash effect
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
+              ),
+            ),
+            Container(
+              width: 175,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(
+                  color: const Color.fromARGB(255, 43, 91, 16),
+                  width: 5,
+                ),
+              ),
+              child: Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("ok");
+                  },
+                  child: Text(
+                    "QUIT",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 43, 91, 16),
+                      fontFamily: 'PixelifySans',
+                      fontSize: 50,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Colors.black, // Set the background color to black
+                    // Set text color (optional, already set in Text)
+                  ).copyWith(
+                    // Disable hover effects
+                    elevation:
+                        MaterialStateProperty.all(0), // No shadow on hover
+                    overlayColor: MaterialStateProperty.all(
+                        Colors.transparent), // No hover effect color change
+                    splashFactory:
+                        NoSplash.splashFactory, // Disable splash effect
+                  ),
+                ),
               ),
             ),
           ],
